0b27e0219687ee2159877af84ba4434992150672
ktan <ktan@atlassian.com>
2019-06-14 09:39:44 +1000

    kims commit

--- hello.js
+++ hello.js
@@ -2,4 +2,6 @@ module.exports = function hello () {
   return 'Hello World!';
 }
 
-/* some code */
+/* this is a comment that goes here.
+This is another line of a comment.
+How about another line of a comment in this code */
