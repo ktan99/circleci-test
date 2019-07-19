1fa54f283877bdef2fae71ec9571cddf32b04a3e
Kim <49014841+ktan99@users.noreply.github.com>
2019-07-19 14:12:12 +1000

    Update hello.js

--- hello.js
+++ hello.js
@@ -5,3 +5,36 @@ module.exports = function hello () {
 /* this is a comment that goes here.
 This is another dsksfljk line of a comment.
 How about another line of a comment in this code */
+
+module.exports = function hello () {
+  return 'Hello World!';
+}
+
+/* this is a comment that goes here.
+This is another dsksfljk line of a comment.
+How about another line of a comment in this code */
+
+
+module.exports = function hello () {
+  return 'Hello World!';
+}
+
+/* this is a comment that goes here.
+This is another dsksfljk line of a comment.
+How about another line of a comment in this code */
+
+module.exports = function hello () {
+  return 'Hello World!';
+}
+
+/* this is a comment that goes here.
+This is another dsksfljk line of a comment.
+How about another line of a comment in this code */
+
+module.exports = function hello () {
+  return 'Hello World!';
+}
+
+/* this is a comment that goes here.
+This is another dsksfljk line of a comment.
+How about another line of a comment in this code */
